5d46d553c1e687db9410f5a5d3db24c87f804214
Nicolas De Loof <nicolas.deloof@gmail.com>
2020-06-24 10:43:05 +0200

    Only validate volume exists if this is a Volume mount
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- loader/validate.go
+++ loader/validate.go
@@ -33,8 +33,11 @@ func checkConsistency(project *types.Project) error {
 			}
 		}
 		for _, volume := range s.Volumes {
-			if _, ok := project.Volumes[volume.Source]; !ok {
-				return errors.Wrap(errdefs.ErrInvalid, fmt.Sprintf("service %q refers to undefined volume %s", s.Name, volume.Source))
+			switch volume.Type {
+			case types.VolumeTypeVolume:
+				if _, ok := project.Volumes[volume.Source]; !ok {
+					return errors.Wrap(errdefs.ErrInvalid, fmt.Sprintf("service %q refers to undefined volume %s", s.Name, volume.Source))
+				}
 			}
 		}
 		for _, secret := range s.Secrets {
