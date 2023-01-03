be170befda31a336a94d248ac1221fceeac834c8
Nicolas De Loof <nicolas.deloof@gmail.com>
2023-01-03 09:12:43 +0100

    accept dashes in variable names
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- dotenv/godotenv_test.go
+++ dotenv/godotenv_test.go
@@ -718,3 +718,11 @@ func TestUTF8BOM(t *testing.T) {
 
 	loadEnvAndCompareValues(t, Load, envFileName, expectedValues, noopPresets)
 }
+
+func TestDash(t *testing.T) {
+	loadEnvAndCompareValues(t, Load, "fixtures/special.env", map[string]string{
+		"VAR-WITH-DASHES":      "dashes",
+		"VAR.WITH.DOTS":        "dots",
+		"VAR_WITH_UNDERSCORES": "underscores",
+	}, noopPresets)
+}
