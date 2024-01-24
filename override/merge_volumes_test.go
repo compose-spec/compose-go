9ef30009f219ffeee654953f76ccec12dce51c6a
Milas Bowman <milas.bowman@docker.com>
2024-01-24 18:47:13 -0500

    chore(merge): add test for volumes
    Signed-off-by: Milas Bowman <milas.bowman@docker.com>
    Signed-off-by: Guillaume Lours <705411+glours@users.noreply.github.com>

---
+++ override/merge_volumes_test.go
@@ -0,0 +1,49 @@
+/*
+   Copyright 2020 The Compose Specification Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package override
+
+import (
+	"testing"
+)
+
+func TestMergeVolumes(t *testing.T) {
+	assertMergeYaml(t, `
+services:
+  test:
+    image: foo
+    volumes:
+     - a:/a
+     - type: bind
+       source: ./b
+       target: /b
+`, `
+services:
+  test:
+    volumes:
+      - c:/c
+`, `
+services:
+  test:
+    image: foo
+    volumes:
+      - a:/a
+      - type: bind
+        source: ./b
+        target: /b
+      - c:/c
+`)
+}
