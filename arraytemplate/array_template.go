4cfd19d6843edc414c89c4fc950c22bc57c90c08
boguszj <kuba.bogusz4@gmail.com>
2025-03-01 22:55:50 +0100

    Support inlined arrays in env variables
    Signed-off-by: boguszj <kuba.bogusz4@gmail.com>

--- arraytemplate/array_template.go
+++ arraytemplate/array_template.go
@@ -1,3 +1,19 @@
+/*
+   Copyright 2020 The Compose Specification Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package arraytemplate
 
 import (
@@ -24,32 +40,40 @@ var (
 )
 
 func Substitute(template string, mapping Mapping) ([]string, error) {
-	matches := ArraySubstitutionPattern.FindStringSubmatch(template)
-	prefix, err := findPrefix(matches)
-
+	arrayName, err := findArrayName(template)
 	if err != nil {
-		return nil, fmt.Errorf("invalid array template: %s", template)
+		return nil, err
+	}
+	inlined, err := getInlined(arrayName, mapping)
+	if err != nil {
+		return nil, fmt.Errorf("could not substitute array template \"%s\":\n%w", template, err)
 	}
 
-	arr := make([]string, 0, maxArraySize)
-	for i := 0; i < maxArraySize; i++ {
-		key := fmt.Sprintf("%s[%d]", prefix, i)
-		value, ok := mapping(key)
-		if !ok {
-			break
-		}
-		arr = append(arr, value)
+	if inlined != nil {
+		return inlined, nil
 	}
 
-	return arr, nil
+	return getIndexed(arrayName, mapping), nil
 }
 
-func findPrefix(matches []string) (string, error) {
+func findArrayName(template string) (string, error) {
+	matches := ArraySubstitutionPattern.FindStringSubmatch(template)
+
+	if len(matches) < 1 {
+		return "", fmt.Errorf("not a valid array template: \"%s\"", template)
+	}
+
+	var arrayName string
 	for _, match := range matches[1:] {
 		if match != "" {
-			return match, nil
+			arrayName = match
+			break
 		}
 	}
 
-	return "", fmt.Errorf("could not find a match")
+	if arrayName == "" {
+		return "", fmt.Errorf("this message suggest an internal error and should never occur; if you see this error, please report it: \"%s\"", template)
+	}
+
+	return arrayName, nil
 }
